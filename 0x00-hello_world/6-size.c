dcd2149ed6956391efba38e27234d9f46555d715
Christian Balogun <cpapextom@gmail.com>
2022-09-08 22:42:49 +0100

    f betty

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+/* Description dj */
+#include <stdio.h>
+/**
+ * main - Entry post
+ * Description: hmm
+ * Return: nothing
+ **/
+int main(void)
+{
+printf("size of char is=%i byte\n", sizeof(char));
+printf("size of int is=%i byte\n", sizof(int));
+printf("size of signed char is=%i byt\n\n\n", sizeof(unsigned char));
+return (0);
+}
