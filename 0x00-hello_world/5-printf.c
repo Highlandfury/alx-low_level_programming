c67e2871cf5ca6affa81713d416edae5552c3f7b
Christian Balogun <cpapextom@gmail.com>
2022-09-08 20:15:20 +0100

    df

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+/* do you want it here */
+#include <stdio.h>
+/**
+ * Description: 'No description'
+ */
+int main(void) /* another one */
+{printf("with proper grammar, but the outcome is a peice of art,\n ");
+return (0);
+}
