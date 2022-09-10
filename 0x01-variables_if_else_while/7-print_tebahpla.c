770dd4cb58870effd1961b4d746731343d641d3e
Christian Balogun <cpapextom@gmail.com>
2022-09-09 18:39:35 -0700

    dfef

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - main function
+* Description: prints the lowercase alphabet in reverse
+* Return: 0
+*/
+int main(void)
+{
+char l;
+for (l = 'z'; l >= 'a'; l--)
+putchar(l);
+putchar('\n');
+return (0);
+}
