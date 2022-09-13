3d136bc435b8d53b73fbc2ac8e1ac14e0f6118f5
Christian Balogun <cpapextom@gmail.com>
2022-09-14 00:12:55 +0100

    adsd

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
-* _isalpha - checks for alphanumerical letters
+* _islower - checks for alphanumerical letters
 * @c: a character to be checked on
+* Description: for _islower
 * Return: returns 0 or 1 depending on condition
 */
-int _islower(int c)
+int _islower(int c) 
 {
 return (c >= 'a' && c <= 'z');
 }
