fe2c289811996348cb85eb57abf4bdf1e6eac0cd
Christian Balogun <cpapextom@gmail.com>
2022-09-13 19:15:12 +0100

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabets.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* main - check the code
+* Description: prints alphabets from a to z in lowercase
+* Return: always 0.
+*/
+void print_alphabet(void)
+{
+int s;
+for (s = 'a'; s <= 'z'; s++)
+{
+_putchar(s);
+}
+_putchar('\n')
+}
