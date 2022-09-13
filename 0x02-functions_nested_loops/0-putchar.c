715002e8ea9acd037134fc084b6cd9d40460f65b
Christian Balogun <cpapextom@gmail.com>
2022-09-13 18:10:40 +0100

    printf

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <main.h>
 /**
 * main - main entery here
 * Description: print _putchar
 * Return: always zero
 */
 int main(void)
-{ printf("_putchar\n");
+{
+char *sh = "_putchar";
+while (*sh)
+{
+_putchar(*su);
+su++;
+}
+_putchar('\n');
 return (0);
 }
