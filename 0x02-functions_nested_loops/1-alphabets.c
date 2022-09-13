c74ea819c530e9a09018a83b097b8e5a95091d9b
Christian Balogun <cpapextom@gmail.com>
2022-09-13 19:43:22 +0100

    alphabets

--- 0x02-functions_nested_loops/1-alphabets.c
+++ 0x02-functions_nested_loops/1-alphabets.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * main - check the code
-* Description: prints alphabets from a to z in lowercase
+* print_alphabet: prints alphabets from a to z in lowercase
 * Return: always 0.
 */
 void print_alphabet(void)
@@ -11,5 +11,5 @@ for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
 }
-_putchar('\n')
+_putchar('\n');
 }
