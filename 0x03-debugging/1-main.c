71731c53042a16cc698e5aff55df728ab122bb3b
Christian Balogun <cpapextom@gmail.com>
2022-09-16 02:40:35 +0100

    sdsf

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,19 +1,24 @@
+#include <stdio.h>
 
-#include <stdio.h>
 /**
- * main - causes an infinite loop.
+ * main - causes an infinite loop
  * Return: 0
- * betty style doc for function main goes there
  */
+
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
+
 i = 0;
-/* while (i < 10) */
-/* { */
-/* putchar(i); */
-/* } */
+
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
+
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
