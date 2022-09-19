7abbef4524c76377adfe19bce75405f11aeb7f8f
Christian Balogun <cpapextom@gmail.com>
2022-09-19 23:41:49 +0100

    sdf

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, 
+ *
+ * @s: string to reverse
+ *
+ *
+ * Return: Always zero.
+ */
+
+void print_rev(char *s)
+{
+	int n, p;
+
+	n = 0;
+
+	while
+		(s[n] != '\0')
+		{
+			i++;
+		}
+	p = n - 1;
+
+	for (n = p; n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+
+	_putchar('\n');
+}
